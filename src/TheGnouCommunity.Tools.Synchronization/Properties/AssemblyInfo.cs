ac6625a6fadd88e103a32e76fdb2a935bd251623
Boulc <boulc@boulc.com>
2016-10-29 14:33:16 -0400

    Add project files.

---
+++ src/TheGnouCommunity.Tools.Synchronization/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCopyright("The Gnou Community (http://www.the-gnou-community.net/)")]
+[assembly: AssemblyProduct("TheGnouCommunity.Tools.Synchronization")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e453b7b8-6670-4160-9cf7-18535f4e8fd8")]
